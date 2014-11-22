827dadd431465c8f25ed7131757d1684006b2be5
kseniya <kseniyalr@gmail.com>
2014-11-22 17:18:30 +0300

    Добавлены представления Преподаватели, Кафедры

--- app/Преподаватели.js
+++ app/Преподаватели.js
@@ -6,4 +6,21 @@ function Преподаватели() {
     var self = this, model = this.model, form = this;
     
     // TODO : place your code here
+
+    function addActionPerformed(evt) {//GEN-FIRST:event_addActionPerformed
+        // TODO Добавьте свой код:
+        model.teacherInform.insert();
+    }//GEN-LAST:event_addActionPerformed
+
+    function SaveActionPerformed(evt) {//GEN-FIRST:event_SaveActionPerformed
+        // TODO Добавьте свой код:
+        model.save();
+    }//GEN-LAST:event_SaveActionPerformed
+
+    function DeleteActionPerformed(evt) {//GEN-FIRST:event_DeleteActionPerformed
+        // TODO Добавьте свой код:
+        if (confirm("удалить?")) {
+            model.teacherInform.deleteRow();
+    }//GEN-LAST:event_DeleteActionPerformed
+}
 }
--- app/Старт.js
+++ app/Старт.js
@@ -6,4 +6,9 @@ function __1() {
     var self = this, model = this.model, form = this;
     
     // TODO : place your code here
+
+    function KeyExtradActionPerformed(evt) {//GEN-FIRST:event_KeyExtradActionPerformed
+        // TODO Добавьте свой код:
+    
+    }//GEN-LAST:event_KeyExtradActionPerformed
 }
