43ed5de48aa1053acc218aa60b8a90395c61639c
TrefilovaNS <mionavlika@ymail.com>
2014-11-23 16:06:25 +0400

    Исправлены конфликты

--- app/qSTATUS.sql
+++ app/qSTATUS.sql
@@ -3,13 +3,6 @@
  * @author Пользователь
  * @name qSTATUS
  */ 
-<<<<<<< HEAD
 Select rnumber, status, room_id, keystatus_id
 From ROOM t1
 , KEYSTATUS t
-=======
-Select rnumber, corp_name, status 
-From KEYSTATUS t1
-, ROOM t
-, CORPUS t2
->>>>>>> gui
