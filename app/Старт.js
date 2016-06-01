fecb58e50362199cb40136fe1eff025e0219c6e0
jskonst <jskonst@yandex.ru>
2016-06-01 22:38:51 +0300

    to new Project

--- app/Старт.js
+++ app/Старт.js
@@ -3,31 +3,38 @@
  * @author ксюша
  * @public
  */
-function MainForm() {
-    var self = this, model = this.model, form = this;
-    
-    // TODO : place your code here
 
-    function KeyExtradActionPerformed(evt) {//GEN-FIRST:event_KeyExtradActionPerformed
-        // TODO Добавьте свой код:
-        var vidWindow = new Vidacha();
-            vidWindow.show();
-    
-    }//GEN-LAST:event_KeyExtradActionPerformed
+define('Launch', ['orm', 'forms', 'ui'], function (Orm, Forms, Ui, ModuleName) {
+    function module_constructor() {
+        var self = this
+                , model = Orm.loadModel(ModuleName)
+                , form = Forms.loadForm(ModuleName, model);
 
-    function WatchInfoActionPerformed(evt) {//GEN-FIRST:event_WatchInfoActionPerformed
-        // TODO Добавьте свой код:
-        var watchI = new WatchInfo();
-        watchI.show();
-    }//GEN-LAST:event_WatchInfoActionPerformed
+//Вот весь этот код ниже надо приводить в божеский вид примерно так
+//        form.KeyExtrad.onActionPerformed = function (evt) {
+////            var vidWindow = new Vidacha();
+////            vidWindow.show();
+//        };
 
-    function ReportActionPerformed(evt) {//GEN-FIRST:event_ReportActionPerformed
-        // TODO Добавьте свой код:
-    }//GEN-LAST:event_ReportActionPerformed
+        self.show = function () {
+            form.show();
+        };
 
-    function EditActionPerformed(evt) {//GEN-FIRST:event_EditActionPerformed
-        // TODO Добавьте свой код:
-        var EditInfo = new Edit();
-        EditInfo.show();
-    }//GEN-LAST:event_EditActionPerformed
-}
+//
+//        function WatchInfoActionPerformed(evt) {//GEN-FIRST:event_WatchInfoActionPerformed
+//            // TODO Добавьте свой код:
+//            var watchI = new WatchInfo();
+//            watchI.show();
+//        }//GEN-LAST:event_WatchInfoActionPerformed
+//
+//        function ReportActionPerformed(evt) {//GEN-FIRST:event_ReportActionPerformed
+//            // TODO Добавьте свой код:
+//        }//GEN-LAST:event_ReportActionPerformed
+//
+//        function EditActionPerformed(evt) {
+//            var EditInfo = new Edit();
+//            EditInfo.show();
+//        }
+    }
+    return module_constructor;
+});
